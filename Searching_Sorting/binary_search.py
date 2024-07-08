57a65feb70782a30f105eca272403560b9fccb7e
Tejasambekar <tejasambekarkumar@gmail.com>
2024-07-08 05:20:58 +0000

    binary_search

---
+++ Searching_Sorting/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(a,target):
+    n=len(a)
+    left=0
+    right=n-1
+    while left<=right:
+        mid=(left+right)//2
+        
+        if target==a[mid]:
+            return mid
+        elif target<a[mid]:
+            right=mid-1
+            print('hi')
+        else:
+            left=mid+1
+    return -1
+a=[1,2,3,4,5,6]
+k=1
+print(binary_search(a,k))
+
