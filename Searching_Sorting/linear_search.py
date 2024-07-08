72f54a5fe8df06e760aa7edee09f8597284616e7
Tejasambekar <tejasambekarkumar@gmail.com>
2024-07-08 04:29:11 +0000

    linear_search

--- Searching_Sorting/linear_search.py
+++ Searching_Sorting/linear_search.py
@@ -1,1 +1,10 @@
 #Linear Search
+def linear_search(a,k):
+    n=len(a)
+    for i in range(n):
+        if k==a[i]:
+            return i 
+    return -1
+a=[1,2,2,24,1,1]
+k=24
+print(linear_search(a,k)) 
